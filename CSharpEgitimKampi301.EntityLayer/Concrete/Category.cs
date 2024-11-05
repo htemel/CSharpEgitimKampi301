574afd710764ede702428e61d4662ae3d569a294
Hüseyin TEMEL <47536922+htemel@users.noreply.github.com>
2024-11-05 16:48:48 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,32 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    internal class Category
+    {
+        public int CategoryId { get; set; } // Category sınıf adını kullanıp Id yazmalıyız yoksa birincil anahtar olarak algılamaz.
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+/*
+ Field-Variable-Property
+ */
+
+/*
+ * sınıfın içinde tanımlanırsa
+ int x; -->Field
+
+*sınıf için de aşağıdaki şekilde tanımlanırsa property
+    public int x{get; set;} -->Property
+
+*sınıf içinde ve metot içinde tanımlanırsa değişken
+    void Test()
+{
+int x
+}
+ */
