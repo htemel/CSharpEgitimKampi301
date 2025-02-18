b3a8c413f2bbe0d6813d096027c0b9c484a1494f
htemel <temel200462@yahoo.com>
2025-02-18 09:49:36 +0300

    Ders 13 Sonu

---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer
+{
+    public interface IGenericDal<T> where T:class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
--- CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
@@ -14,6 +14,8 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public string CustomerDistrict { get; set; }
         public string CustomerCity { get; set; }
         public List<Order> Orders { get; set; }// bu tabloya sen Order tablosu ile çalışcaksın diyoruz.
-        
+        public bool CustomerStatus { get; set; } // burayı sonra ekledik bunu veritabanına yansıtmak için console ekranında "add-migration 'mig1' " ile yeni bir migration ekliyecez sonra veritabanını güncelleyecez. Bunu console da DataAccessLayer seçip yapacaz.
+        // mig1 migration oluştuktan sonra veritabanına yanstımak için console ekranında "update-database" çalıştıracaz
+
     }
 }
