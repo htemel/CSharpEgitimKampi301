3428524a559f5873e8fd6b30bbd4e74f7f60cf0d
htemel <temel200462@yahoo.com>
2025-03-19 13:21:25 +0300

    Ders 19 Sonu

--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -41,6 +41,7 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnGetById = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -160,6 +161,15 @@
             this.btnGetById.Text = "ID\'ye Göre Getir";
             this.btnGetById.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-15, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FrmCategory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -176,6 +186,7 @@
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnList);
             this.Controls.Add(this.label1);
             this.Name = "FrmCategory";
@@ -201,6 +212,7 @@
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.Button button1;
     }
 }
 
